36e6b815d0e5c7850054e160e8dc0b6c630c6c00
geekbrains <info@geebrains.ru>
2022-02-20 12:49:44 +0300

    Initial commit

---
+++ src/lesson4/Main.java
@@ -0,0 +1,130 @@
+package lesson4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class TicTacToe {
+
+    Random random;
+    Scanner scanner;
+    char[][] table;
+
+    public static void main(String[] args) {
+        new TicTacToe().game();
+    }
+
+    TicTacToe() {
+        random = new Random();
+        scanner = new Scanner(System.in);
+        table = new char[3][3];
+    }
+
+    private void game() {
+        initTable();
+        printTable();
+        while (true) {
+            turnHuman();
+            if (checkWin('x')) {
+                System.out.println("YOU WON!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("Sorry, DRAW...");
+                break;
+            }
+            turnAI();
+            printTable();
+            if (checkWin('0')) {
+                System.out.println("AI WON!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("Sorry, DRAW...");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
+        printTable();
+    }
+
+    void initTable() {
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                table[x][y] = '.';
+            }
+        }
+    }
+
+    void printTable() {
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                System.out.print(table[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void turnHuman() {
+        int x, y;
+        do {
+            System.out.print("Enter x y [1..3]:");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        table[x][y] = 'x';
+    }
+
+    void turnAI() {
+        int x, y;
+        do {
+            x = random.nextInt(3);
+            y = random.nextInt(3);
+        } while (!isCellValid(x, y));
+        table[x][y] = '0';
+    }
+
+    boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x > 2 || y > 2) {
+            return false;
+        }
+        return table[x][y] == '.';
+    }
+
+    boolean checkWin(char ch) {
+        //
+        int x, y;
+        for (x = 0; x < 2; x++) {
+            for (y = 0; y < 2; y++) {
+                if (table[y][0 + x] == ch && table[y][1 + y] == ch) {
+                    return true;
+                }
+            }
+        }
+        //
+        for (x = 0; x < 2; x++) {
+            for (y = 0; y < 2; y++) {
+                if (table[0 + x][y] == ch && table[1 + x][y] == ch) {
+                    return true;
+                }
+            }
+        }
+        //
+        if (table[0][0] == ch && table[1][1] == ch && table[2][2] == ch) return true;
+        if (table[2][0] == ch && table[1][1] == ch && table[0][2] == ch) return true;
+        return false;
+    }
+
+    boolean isTableFull() {
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                if (table[x][y] == '.') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
+
